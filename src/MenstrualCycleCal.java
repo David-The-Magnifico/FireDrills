3c9db379034a48ab9edcde4016458d63d55b8aab
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-12 18:50:27 +0100

    commit to new DIR

--- src/MenstrualCycleCal.java
+++ src/MenstrualCycleCal.java
@@ -6,3 +6,6 @@ public class MenstrualCycleCal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Name:");
+        String name = scanner.nextLine();
+
