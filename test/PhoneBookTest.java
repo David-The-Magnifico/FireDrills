6f4553a8177507fad1a2005b8af0689a5fb01fe4
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-12 23:18:09 +0100

    commit to update test

--- test/PhoneBookTest.java
+++ test/PhoneBookTest.java
@@ -1,1 +1,42 @@
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class PhoneBookTest {
+
+    @Test
+    void addContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        String contactDetails = "David Oladipo\n123-456-7890\noladipo.david@outlook.com\nNVIDIA Inc.\n154 Trident Ln.\nSilicon Valley, CA";
+        phoneBook.addContact(contactDetails);
+        assertEquals(1, phoneBook.contacts.size());
+        assertEquals(contactDetails, phoneBook.getFirst());
+    }
+
+    @Test
+    void viewContacts() {
+    PhoneBook phoneBook = new PhoneBook();
+    phoneBook.contacts.add("David Oladipo\n123-456-7890\noladipo.david@outlook.com\nNVIDIA Inc.\n154 Trident Ln.\nSilicon Valley, CA");
+    phoneBook.viewContacts();
+    assertEquals(1, phoneBook.contacts.size());
+    }
+
+    @Test
+void editContact() {
+    PhoneBook phoneBook = new PhoneBook();
+    phoneBook.contacts.add("David Oladipo\n123-456-7890\noladipo.david@outlook.com\nNVIDIA Inc.\n154 Trident Ln.\nSilicon Valley, CA");
+    String oldContact = "David Oladipo";
+    String newContact = "David Magnifico";
+    phoneBook.editContact(oldContact, newContact);
+    assertEquals("David Magnifico\n123-456-7890\noladipo.david@outlook.com\nNVIDIA Inc.\n154 Trident Ln.\nSilicon Valley, CA", phoneBook.getFirst());
+}
+
+    @Test
+    void deleteContact() {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.contacts.add("David Oladipo\n123-456-7890\noladipo.david@outlook.com\nNVIDIA Inc.\n154 Trident Ln.\nSilicon Valley, CA");
+        String contactToDelete = "David Oladipo";
+        phoneBook.deleteContact(contactToDelete);
+        assertEquals("Phone book is empty", phoneBook.getFirst());
+    }
+}
