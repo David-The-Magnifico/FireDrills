e98b3f8375f7bf80428ff04be92e2c6d9b548044
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-13 08:08:42 +0100

    commit to mind game

---
+++ src/TicTacToeMindGame/Player.java
@@ -0,0 +1,19 @@
+package TicTacToeMindGame;
+
+public class Player {
+        private String name;
+        private Cell cell;
+        public Player(String name, Cell cell){
+            this.name = name;
+            this.cell = cell;
+        }
+        public Cell getCell(){
+            return cell;
+        }
+        public String getName(){
+            return name;
+        }
+
+
+
+}
