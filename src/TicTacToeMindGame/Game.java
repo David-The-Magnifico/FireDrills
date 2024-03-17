5dfb85ef0e413412394de6ef63b8e95bbc80135b
David-The-Magnifico <www.wealthydavid@gmail.com>
2024-03-17 16:41:35 +0100

    commit to mind game

---
+++ src/TicTacToeMindGame/Game.java
@@ -0,0 +1,11 @@
+package TicTacToeMindGame;
+
+import static TicTacToeMindGame.TicTacToe.*;
+
+public class Game {
+    public static void main(String[] args) {
+        initializeBoard(board);
+        displayBoard();
+        playGame();
+        }
+}
